c5b7f220222e9080a2731e565d638704c261ab13
Clinton Ribeiro <96874345+Domclinton1@users.noreply.github.com>
2023-10-05 14:51:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function casarao() {
+    var click = window.document.getElementById('li#ctt')
+    window.open("https://casaraotemdetudo.com", '_blank')
+}
+function vidraça() {
+    var click = window.document.getElementById('li#vidro')
+    window.open("https://www.instagram.com/vidracaria_casaraodevidro/", '_blank')
+}
+function sabor() {
+    var click = window.document.getElementById('li#sdq')
+    window.open("https://www.instagram.com/sabordaqualidade/", '_blank')
+}
+function fit() {
+    var click = window.document.getElementById('li#forma')
+    window.open("https://www.instagram.com/toemforma_fitness/", '_blank')
+}
+function myinsta() {
+    var click = window.document.getElementById('div#insta')
+    window.open("https://www.instagram.com/domclintonii/", '_blank')
+}
+function mygit() {
+    var click = window.document.getElementById('div#Github')
+    window.open("https://github.com/Domclinton1", '_blank')
+}
+function mylinkedin() {
+    var click = window.document.getElementById('div#linkedin')
+    window.open("https://www.linkedin.com/in/clinton-ribeiro-964790242/", '_blank')
+}
